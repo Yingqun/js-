349bb108337854fbf0a0568fd3401174fdabc314
liuyingqun <liuyingqun@sensetime.com>
2021-02-08 14:33:42 +0800

    2021-02-08  1365. 有多少小于当前数字的数字

---
+++ 哈希表/1365.有多少小雨当前数字的数字.js
@@ -0,0 +1,17 @@
+// https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+// 输入：nums = [8,1,2,2,3]
+// 输出：[4,0,1,1,3]
+// 解释： 
+// 对于 nums[0]=8 存在四个比它小的数字：（1，2，2 和 3）。 
+// 对于 nums[1]=1 不存在比它小的数字。
+// 对于 nums[2]=2 存在一个比它小的数字：（1）。 
+// 对于 nums[3]=2 存在一个比它小的数字：（1）。 
+// 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
+
+//第一遍
+var smallerNumbersThanCurrent = function(nums) {
+    return nums.map(n => nums.reduce((a, b) =>  a + (b > n ? 1 : 0), 0))
+};
+
+console.log(smallerNumbersThanCurrent([8,1,2,2,3]))
