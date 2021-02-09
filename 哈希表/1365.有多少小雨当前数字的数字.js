6dbf1398defe77c37e7c787ff6ee926ca4092565
liuyingqun <liuyingqun@sensetime.com>
2021-02-09 13:43:25 +0800

    1.two sum

--- 哈希表/1365.有多少小雨当前数字的数字.js
+++ 哈希表/1365.有多少小雨当前数字的数字.js
@@ -10,14 +10,27 @@
 // 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
 
 //第一遍
-var smallerNumbersThanCurrent = function(nums) {
-    return nums.map(n => nums.reduce((a, b) =>  a + (n > b ? 1 : 0), 0))
-};
+// var smallerNumbersThanCurrent = function(nums) {
+//     return nums.map(n => nums.reduce((a, b) =>  a + (n > b ? 1 : 0), 0))
+// };
 
+// var smallerNumbersThanCurrent2 = function(nums) {
+//     const sortArray = [...nums].sort((a, b) => b - a)
+//     const map = new Map(sortArray.map((num, index) => [num, nums.length - index - 1]))
+//     return nums.map((num) => map.get(num))
+// }
+
+
+//第二遍
+var smallerNumbersThanCurrent = function(nums) {
+    return nums.map(n => nums.reduce((a, b)=> a = a + (b < n ? 1 : 0),0))
+}
 var smallerNumbersThanCurrent2 = function(nums) {
+    // nums 和 [...nums]的结果不一样
     const sortArray = [...nums].sort((a, b) => b - a)
-    const map = new Map(sortArray.map((num, index) => [num, nums.length - index - 1]))
-    return nums.map((num) => map.get(num))
+    console.log(sortArray)
+    const map = new Map(sortArray.map((item, index) => [item, nums.length - index - 1]))
+    return nums.map(item => map.get(item))
 }
 
 console.log(smallerNumbersThanCurrent([8,1,2,2,3]))
---
+++ 数组/1.两数字之和.js
@@ -0,0 +1,27 @@
+// https://leetcode-cn.com/problems/two-sum/submissions/
+
+var twoSum1 = function(nums, target) { 
+    let map = new Map()
+    for(let i = 0; i < nums.length; i++) {
+       if(map.has(target - nums[i])) {
+           return [map.get(target - nums[i]), i]
+       }else {
+           map.set(nums[i], i)
+       }
+    }
+    return []
+};
+var twoSum2 = function(nums, target) { 
+    let map = {}
+    for(let i = 0; i < nums.length; i++) {
+       if(map[target - nums[i]] !== undefined) {
+           return [map[target - nums[i]], i]
+       }else {
+           map[nums[i]] = i
+       }
+    }
+    return []
+};
+const nums = [2,7,11,15]
+console.log(twoSum1(nums, 9))
+console.log(twoSum2(nums, 9))
