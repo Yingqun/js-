ff8957d6a49901f066893b9b5cd03db8ebe82c05
liuyingqun <liuyingqun@zhongan.com>
2021-03-26 23:31:08 +0800

    11.盛水最多的容器

---
+++ 数组/11.盛水最多的容器.js
@@ -0,0 +1,17 @@
+// https://leetcode-cn.com/problems/container-with-most-water/
+
+// 输入：height = [4,3,2,1,4]
+// 输出：16
+
+var maxArea = function (height) {
+  let result = 0, left = 0, right = height.length - 1;
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    result = Math.max(result, smallestSide * (right - left));
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return result;
+}
+
+console.log(maxArea([4, 3, 2, 1, 4]))
