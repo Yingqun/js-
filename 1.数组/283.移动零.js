56cf8adcf5cc4ae20037c7d48a5058cef95f5acd
liuyingqun <liuyingqun@zhongan.com>
2021-04-02 16:15:42 +0800

    移动零 选择排休 分治回溯

--- 1.数组/283.移动零.js
+++ 1.数组/283.移动零.js
@@ -12,10 +12,9 @@ var moveZeroes = function (nums) {
   let j = 0
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] != 0) {
+      let temp = nums[j]
       nums[j] = nums[i]
-      if (i !== j) {
-        nums[i] = 0
-      }
+      nums[i] = temp
       j++
     }
   }
